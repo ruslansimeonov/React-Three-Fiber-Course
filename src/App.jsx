4d15448b6ea0db099a099f03769cfcfa9028c223
Ruslan Simeonov <ruslan.simeonov@alten.nl>
2023-07-17 11:19:57 +0200

    Lights

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,112 @@
-import { Canvas } from '@react-three/fiber'
-import Polyhedron from './Polyhedron'
-import * as THREE from 'three'
-import { Stats, OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
-import { useRef } from 'react'
+import { Canvas } from '@react-three/fiber';
+import Polyhedron from './Polyhedron';
+import * as THREE from 'three';
+import { Stats, OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
+import { useRef } from 'react';
 
 function Lights() {
-  const ambientRef = useRef()
-  const directionalRef = useRef()
-  const pointRef = useRef()
-  const spotRef = useRef()
+  const ambientRef = useRef();
+  const directionalRef = useRef();
+  const pointRef = useRef();
+  const spotRef = useRef();
 
   useControls('Ambient Light', {
     visible: {
       value: false,
       onChange: (v) => {
-        ambientRef.current.visible = v
-      },
+        ambientRef.current.visible = v;
+      }
     },
     color: {
       value: 'white',
       onChange: (v) => {
-        ambientRef.current.color = new THREE.Color(v)
-      },
-    },
-  })
+        ambientRef.current.color = new THREE.Color(v);
+      }
+    }
+  });
 
   useControls('Directional Light', {
     visible: {
       value: true,
       onChange: (v) => {
-        directionalRef.current.visible = v
-      },
+        directionalRef.current.visible = v;
+      }
     },
     position: {
       x: 1,
       y: 1,
       z: 1,
       onChange: (v) => {
-        directionalRef.current.position.copy(v)
-      },
+        directionalRef.current.position.copy(v);
+      }
     },
     color: {
       value: 'white',
       onChange: (v) => {
-        directionalRef.current.color = new THREE.Color(v)
-      },
-    },
-  })
+        directionalRef.current.color = new THREE.Color(v);
+      }
+    }
+  });
 
   useControls('Point Light', {
     visible: {
       value: false,
       onChange: (v) => {
-        pointRef.current.visible = v
-      },
+        pointRef.current.visible = v;
+      }
     },
     position: {
       x: 2,
       y: 0,
       z: 0,
       onChange: (v) => {
-        pointRef.current.position.copy(v)
-      },
+        pointRef.current.position.copy(v);
+      }
     },
     color: {
       value: 'white',
       onChange: (v) => {
-        pointRef.current.color = new THREE.Color(v)
-      },
-    },
-  })
+        pointRef.current.color = new THREE.Color(v);
+      }
+    }
+  });
 
   useControls('Spot Light', {
     visible: {
       value: false,
       onChange: (v) => {
-        spotRef.current.visible = v
-      },
+        spotRef.current.visible = v;
+      }
     },
     position: {
       x: 3,
       y: 2.5,
       z: 1,
       onChange: (v) => {
-        spotRef.current.position.copy(v)
-      },
+        spotRef.current.position.copy(v);
+      }
     },
     color: {
       value: 'white',
       onChange: (v) => {
-        spotRef.current.color = new THREE.Color(v)
-      },
-    },
-  })
+        spotRef.current.color = new THREE.Color(v);
+      }
+    }
+  });
 
   return (
     <>
       <ambientLight ref={ambientRef} />
       <directionalLight ref={directionalRef} />
-      <pointLight ref={pointRef} />
+      <pointLight ref={pointRef}>
+        <mesh>
+          <sphereGeometry args={[0.2]}></sphereGeometry>
+        </mesh>
+      </pointLight>
       <spotLight ref={spotRef} />
     </>
-  )
+  );
 }
 
 export default function App() {
@@ -134,7 +138,7 @@ export default function App() {
         material={
           new THREE.MeshStandardMaterial({
             color: 0xff0033,
-            flatShading: true,
+            flatShading: true
           })
         }
       />
@@ -143,5 +147,5 @@ export default function App() {
       <gridHelper />
       <Stats />
     </Canvas>
-  )
+  );
 }
